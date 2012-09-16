52bf4697078565e309f2c41ab20208e9b6efb985
Chuck Yang <yangchuck@gmail.com>
2012-09-16 15:39:40 -0400

    first commit of everything

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 103 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#103 PREEMPT Fri Aug 10 15:14:56 BST 2012"
+#define LINUX_COMPILE_TIME "15:14:56"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "webserv"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.14-LeeDrOiD-HC-V2.1.0-"
